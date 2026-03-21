133d79321edf930bb882583598275a153bc7f3cd
NicholasLupo <nicholaslupo14@gmail.com>
2026-03-21 16:01:40 +0100

    add array of objects

---
+++ script.js
@@ -0,0 +1,25 @@
+//Snack 1 - Il peso delle bici
+//Array di oggetti
+const bikes = [
+
+    {
+        name: 'Bici da corsa',
+        peso: '8kg'
+    },
+    {
+        name: 'Mountain bike',
+        peso: '12kg'
+    },
+    {
+        name: 'Bicicletta elettrica',
+        peso: '23kg'
+    },
+    {
+        name: 'Bici da città',
+        peso: '15kg'
+    },
+    {
+        name: 'Bici da bambini',
+        peso: '4kg'
+    },
+]
